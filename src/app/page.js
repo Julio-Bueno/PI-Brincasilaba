f3dd7f5d3f4d1602bb8487ec7fc7f7a9e3616a3e
julio-bueno <julioleobuueno@gmail.com>
2025-09-30 21:55:57 -0300

    modified Brincasilaba page

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,16 @@
 
 export default function Home() {
   return (
-    <div>
-    <h1>BrincaSilaba</h1>
-    </div>
+    < >
+      <div  className = "flex items-center justify-center h-screen text-6xl font-bold">
+        BrincaSilaba
+
+        <a href = "../game"><button className="ml-4 px-4 py-2 bg-white text-black text-4xl rounded hover:bg-gray-700 transition duration-300 "> 
+          Jogar!
+        </button></a>
+        
+        <a href = "../about"><button className="ml-4 px-4 py-2 bg-white text-black text-4xl rounded hover:bg-gray-700 transition dutation-300 ">Sobre </button></a>
+      </div>
+    </>
   );
 }
